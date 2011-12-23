09f754e91f764680069c0fe4cd85a62cb0525641
Jacob Rief <jacob.rief@gmail.com>
2011-12-23 23:49:23 +0100

    Renamed VariableProductDetail -> VariableProductDetailView

--- tests/testapp/project/views.py
+++ tests/testapp/project/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import redirect
-from shop_productvariations.views import VariableProductDetail
+from shop_productvariations.views import VariableProductDetailView
 from models import DiaryProduct
 
 
-class DiaryDetailView(VariableProductDetail):
+class DiaryDetailView(VariableProductDetailView):
     """
     This view handles displaying the detail view for test product Diary 
     """
