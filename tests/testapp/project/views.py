11c6d0d00791111c2aaef8c1209d2b481fcee0cc
Jacob Rief <jacob.rief@gmail.com>
2012-01-21 18:27:14 +0100

    Test views honor MixinViews now

--- tests/testapp/project/views.py
+++ tests/testapp/project/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import redirect
-from shop_productvariations.views import VariableProductDetailView
+from shop.views.product import ProductDetailView
+from shop_productvariations.views import ProductDetailViewMixin
 from models import DiaryProduct
 
 
-class DiaryDetailView(VariableProductDetailView):
+class DiaryDetailView(ProductDetailViewMixin, ProductDetailView):
     """
     This view handles displaying the detail view for test product Diary 
     """
